2efe9aad3405fd383bfaf71e09318b536ff277d1
Michiel Borkent <michielborkent@gmail.com>
2022-10-11 12:18:23 +0200

    wip

--- bb/tasks.clj
+++ bb/tasks.clj
@@ -2,7 +2,7 @@
   (:require [quickdoc.api :refer [quickdoc]]))
 
 (defn test-root [_]
-  (quickdoc {:source-paths ["../clj-kondo/src"]
+  (quickdoc {:source-paths ["../clj-kondo/src/clj_kondo/core.clj"]
              :outfile "test/clj-kondo.md"
              :github/repo "https://github.com/clj-kondo/clj-kondo"
              :git/branch "master"})
